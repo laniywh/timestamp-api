3b2e236bcf1720d927d021a30b41cd87a76df3cc
Lani Huang <laniywh@gmail.com>
2017-04-22 09:44:20 -0700

    add index and style

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 });
 
 app.get('/:input', function (req, res) {
@@ -14,6 +18,7 @@ app.get('/:input', function (req, res) {
     var string = req.params.input;
     var date;
 
+    // only numbers
     if (!string.match(/\D/) && (date = moment.utc(string, 'X')).isValid()) {
 
         res.json({
