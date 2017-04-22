819e05d3e4fe0d272d25eb328d6758b405ef9ba9
Lani Huang <laniywh@gmail.com>
2017-04-21 23:14:18 -0700

    add unix and natural time parsing function

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/:input', function (req, res) {
+
+    var string = req.params.input;
+    var date;
+
+    if (!string.match(/\D/) && (date = moment.utc(string, 'X')).isValid()) {
+
+        res.json({
+            "unix": parseInt(date.format('X')),
+            "natural": date.format('MMMM DD, YYYY')
+        });
+
+    } else if((date = moment.utc(string, 'MMMM DD, YYYY')).isValid()) {
+
+        res.json({
+            "unix": parseInt(date.format('X')),
+            "natural": date.format('MMMM DD, YYYY')
+        });
+
+    } else {
+        res.json({
+            "unix": null,
+            "natural": null
+        });
+    }
+});
+
+app.listen(port);
